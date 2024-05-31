7dcea8c2d2d5325e20b26d4ca953a5feac81024a
DaviHVL <149121542+DaviHVL@users.noreply.github.com>
2024-05-31 14:28:46 -0300

    Testes

--- main.cpp
+++ main.cpp
@@ -9,26 +9,29 @@ using namespace std;
 //teste classes
 
 int main(){
-    /*CodigoPagamento teste ;
+
+    CodigoPagamento teste ;
     int a;
     cin>>a;
     try{
         teste.setCodigoP(a);
-        cout<<"Seu codigo e: "<<teste.getCodigoP()<<endl;
+        cout<<"Seu codigoPag e: "<<teste.getCodigoP()<<endl;
     } catch (const invalid_argument& e){
         cout<<e.what()<<endl;
     };
 
+    
     CodigoTitulo teste1;
     string b;
     cin>>b;
     try{
         teste1.setCodigoT(b);
-        cout<<"Seu codigo e: "<<teste1.getCodigoT()<<endl;
+        cout<<"Seu codigoTit e: "<<teste1.getCodigoT()<<endl;
     } catch (const invalid_argument& e){
         cout<<e.what()<<endl;
     };
 
+    
     CPF teste2;
     string c;
     cin>>c;
@@ -37,8 +40,9 @@ int main(){
         cout<<"Seu CPF e: "<<teste2.getCPF()<<endl;
     } catch (const invalid_argument& e){
         cout<<e.what()<<endl;
-    };*/
+    };
 
+    
     Data teste3;
     string d;
     cin>>d;
@@ -48,10 +52,75 @@ int main(){
     } catch(const invalid_argument& e){
         cout<<e.what()<<endl;
     };
+
+    
+    Dinheiro teste4;
+    float e;
+    cin>>e;
+    try{
+        teste4.setDinheiro(e);
+        cout<<"O dinheiro e: "<<teste4.getDinheiro()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
+    
+    Estado teste5;
+    string f;
+    cin>>f;
+    try{
+        teste5.setEstado(f);
+        cout<<"O estado e: "<<teste5.getEstado()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
+
+
+    Nome teste6;
+    string g;
+    getline(cin, g);
+    try{
+        teste6.setNome(g);
+        cout<<"O nome e: "<<teste6.getNome()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
+
+    Percentual teste7;
+    int h;
+    cin>>h;
+    try{
+        teste7.setPercentual(h);
+        cout<<"O percentual e: "<<teste7.getPercentual()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
+
+    Senha teste8;
+    string i;
+    cin>>i;
+    try{
+        teste8.setSenha(i);
+        cout<<"A senha e: "<<teste8.getSenha()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
+
+    Setor teste9;
+    string j;
+    cin>>j;
+    try{
+        teste9.setSetor(j);
+        cout<<"O setor e: "<<teste9.getSetor()<<endl;
+    } catch(const invalid_argument& e){
+        cout<<e.what()<<endl;
+    };
+
     return 0;
 
-
-
 }
-
 
