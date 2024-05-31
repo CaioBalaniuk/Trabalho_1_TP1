697e12bbcb18c2c012dbe663c9ea7d42b7208789
DaviHVL <149121542+DaviHVL@users.noreply.github.com>
2024-05-31 14:33:09 -0300

    Parte Caio 1 (Entidades)

--- entidades.h
+++ entidades.h
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-//Declara��o da Classe Pagamento
+//Declaração da Classe Pagamento
 class Pagamento{
 private:
     CodigoPagamento codigo;
@@ -17,6 +17,21 @@ private:
 public:
     void setPagamento(int novo_codigo, string nova_data, int novo_percentual, string novo_estado);
     tuple<int, string, int, string> getPagamento();
+};
+
+//Declaração da Classe Titulo;
+class Titulo {
+    private:
+        CodigoTitulo codigo;
+        Nome emissor;
+        Setor setor;
+        Data emissao;
+        Data vencimento;
+        Dinheiro valor;
+    public:
+        void setTitulo (string, string, string, string, string, int);
+        vector<string> getTitulo();
+
 };
 
 #endif // ENTIDADES_H_INCLUDED
