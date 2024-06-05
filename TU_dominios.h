5230c154550ea43dd2a35ecb2336879e9df0f98b
DaviHVL <149121542+DaviHVL@users.noreply.github.com>
2024-06-05 19:10:52 -0300

    Partezinha 1

--- TU_dominios.h
+++ TU_dominios.h
@@ -17,9 +17,29 @@ public:
     const static bool SUCESSO = true;
     const static bool FALHA = false;
     void run();
-
-
 };
 
+
+//Declaração da Classe de Teste para o Percentual
+class TUPercentual{
+private:
+    const static int valido = 50;
+    const static int invalido = 150;
+
+    Percentual* ponteiro_percentual;
+
+    bool estado;
+
+    void inicio();
+    void caso_sucesso();
+    void caso_falha();
+    void fim();
+
+public:
+    const static bool sucesso = true;
+    const static bool falha = false;
+
+    void run();
+};
 
 #endif // TU_DOMINIOS_H_INCLUDED
