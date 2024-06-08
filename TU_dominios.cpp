e635bd667c62c4f08aac562b33f440bac9226c92
CaioBalaniuk <135898371+CaioBalaniuk@users.noreply.github.com>
2024-06-08 15:25:52 -0300

    Adicionando as minhas classes de teste (CAIO)

--- TU_dominios.cpp
+++ TU_dominios.cpp
@@ -9,8 +9,7 @@ void TU_CodigoP::caso_sucesso() {
     try {
         codigo.setCodigoP(VALOR_VALIDO);
         if (codigo.getCodigoP() == VALOR_VALIDO) {
-            cout<<"SUCESSO - CODIGO: "<<codigo.getCodigoP()<<" e valido."<<endl;
-        } else {
+            cout<<"SUCESSO - CODIGO PAGAMENTO: "<<codigo.getCodigoP()<<" e valido."<<endl;
         }
     } catch (const invalid_argument& e) {
         cout<<"FALHA -"<<e.what()<<endl;
@@ -21,15 +20,104 @@ void TU_CodigoP::caso_falha() {
     try {
         codigo.setCodigoP(VALOR_INVALIDO);
     } catch (const invalid_argument& e) {
-        cout<<"FALHA - CODIGO: "<<VALOR_INVALIDO<<" e invalido."<<endl;
+        cout<<"FALHA - CODIGO PAGAMENTO: "<<VALOR_INVALIDO<<" e invalido."<<endl;
     }
 }
 
 void TU_CodigoP::run() {
-    caso_falha();
     caso_sucesso();
+    caso_falha();
 }
 
+//Implementação metodos da clase TU_CodigoT;
+
+void TU_CodigoT::casos_sucesso() {
+    try {
+        codigo.setCodigoT(VALOR_VALIDO);
+        if (codigo.getCodigoT() == VALOR_VALIDO) {
+            cout<<"SUCESSO - CODIGO TITULO: "<<codigo.getCodigoT()<<" e valido."<<endl;
+        }
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - "<<e.what()<<endl;
+    }
+}
+
+void TU_CodigoT::casos_falha() {
+    try {
+        codigo.setCodigoT(VALOR_INVALIDO);
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - CODIGO TITULO: "<<VALOR_INVALIDO<<" e invalido."<<endl;
+    }
+}
+
+void TU_CodigoT::runn() {
+    casos_sucesso();
+    casos_falha();
+}
+
+//Implementação metodos da classe TU_CPF;
+
+void TU_CPF::caso_sucesso() {
+    try {
+        cpf.setCPF(VALOR_VALIDO);
+        if (cpf.getCPF() == VALOR_VALIDO) {
+            cout<<"SUCESSO - CPF: "<<cpf.getCPF()<<" e valido."<<endl;
+        }
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - "<<e.what()<<endl;
+    }
+}
+
+void TU_CPF::caso_falha() {
+    try {
+        cpf.setCPF(VALOR_INVALIDO);
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - CPF: "<<VALOR_INVALIDO<<" e invalido."<<endl;
+    }
+}
+
+void TU_CPF::run() {
+    caso_sucesso();
+    caso_falha();
+}
+
+//Implementação metodos da Classe TU_DATA;
+
+void TU_DATA::start(){
+    data = new Data();
+}
+
+void TU_DATA::fim() {
+    delete data;
+}
+
+void TU_DATA::caso_sucesso() {
+    start();
+    try {
+        data->setData(VALOR_VALIDO);
+        if (data->getData() == VALOR_VALIDO) {
+            cout<<"SUCESSO - DATA: "<<data->getData()<<" e valida."<<endl;
+        }
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - "<<e.what()<<endl;
+    }
+    fim();
+}
+
+void TU_DATA::caso_falha() {
+    start();
+    try {
+        data->setData(VALOR_INVALIDO);
+    } catch (const invalid_argument& e) {
+        cout<<"FALHA - DATA: "<<VALOR_INVALIDO<<" e invalida."<<endl;
+    }
+    fim();
+}
+
+void TU_DATA::run() {
+    caso_sucesso();
+    caso_falha();
+}
 
 //Implementação da Classe de Teste para o Percentual
 void TUPercentual::inicio(){
@@ -45,7 +133,7 @@ void TUPercentual::caso_sucesso(){
     try{
         ponteiro_percentual->setPercentual(valido);
         if (ponteiro_percentual->getPercentual()==valido){
-            cout << "SUCESSO - CODIGO: " << valido << " e valido." <<endl;
+            cout << "SUCESSO - PERCENTUAL: " << valido << " e valido." <<endl;
         } else {
             estado = falha;
         }
@@ -59,7 +147,7 @@ void TUPercentual::caso_falha(){
         ponteiro_percentual->setPercentual(invalido);
         estado = falha;
     } catch(invalid_argument e){
-        cout << "FALHA - CODIGO: " << invalido << " e invalido." << endl;
+        cout << "FALHA - PERCENTUAL: " << invalido << " e invalido." << endl;
 
     }
 
@@ -74,7 +162,7 @@ void TUPercentual::run(){
 
 
 //Implementação da Classe de Teste para o Nome
-void TUNome::inicio(){
+void TUNome::inicio() {
     ponteiro_nome = new Nome();
     estado = sucesso;
 }
@@ -87,7 +175,7 @@ void TUNome::caso_sucesso(){
     try{
         ponteiro_nome->setNome(valido);
         if (ponteiro_nome->getNome()==valido){
-            cout << "SUCESSO - CODIGO: " << valido << " e valido." <<endl;
+            cout << "SUCESSO - NOME: " << valido << " e valido." <<endl;
         } else {
             estado = falha;
         }
@@ -101,7 +189,7 @@ void TUNome::caso_falha(){
         ponteiro_nome->setNome(invalido);
         estado = falha;
     } catch(invalid_argument e){
-        cout << "FALHA - CODIGO: " << invalido << " e invalido." << endl;
+        cout << "FALHA - NOME: " << invalido << " e invalido." << endl;
 
     }
 
@@ -129,7 +217,7 @@ void TUSenha::caso_sucesso(){
     try{
         ponteiro_senha->setSenha(valido);
         if (ponteiro_senha->getSenha()==valido){
-            cout << "SUCESSO - CODIGO: " << valido << " e valido." <<endl;
+            cout << "SUCESSO - SENHA: " << valido << " e valido." <<endl;
         } else {
             estado = falha;
         }
@@ -143,7 +231,7 @@ void TUSenha::caso_falha(){
         ponteiro_senha->setSenha(invalido);
         estado = falha;
     } catch(invalid_argument e){
-        cout << "FALHA - CODIGO: " << invalido << " e invalido." << endl;
+        cout << "FALHA - SENHA: " << invalido << " e invalido." << endl;
 
     }
 
