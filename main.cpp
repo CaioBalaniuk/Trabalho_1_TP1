6bb812b2a006faadea7308eaf04e82baa0a806c1
CaioBalaniuk <135898371+CaioBalaniuk@users.noreply.github.com>
2024-06-21 13:00:39 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,35 +15,49 @@ using namespace std;
 //teste classes
 
 int main(){
+    cout<<"---------------------------------------------------CLASSES TESTE DOMINIOS-----------------------------------------------"<<endl;
+    cout<<endl;
     TU_CodigoP teste1;
     teste1.run();
+    cout<<endl;
 
     TU_CodigoT teste2;
     teste2.runn();
+    cout<<endl;
 
     TU_CPF teste3;
     teste3.run();
+    cout<<endl;
 
     TU_DATA teste4;
     teste4.run();
+    cout<<endl;
 
     TUPercentual teste5;
     teste5.run();
+    cout<<endl;
 
     TUNome teste6;
     teste6.run();
+    cout<<endl;
 
     TUSenha teste7;
     teste7.run();
+    cout<<endl;
 
     TU_Setor teste8;
     teste8.run();
+    cout<<endl;
 
     TU_Estado teste9;
     teste9.run();
+    cout<<endl;
 
     TU_Dinheiro teste10;
     teste10.run();
+    cout<<endl;
+    cout<<"-------------------------------------------CLASSES DE TESTE ENTIDADES---------------------------------------------------";
+    cout<<endl;
 
     TUPagamento teste11;
     teste11.run();
